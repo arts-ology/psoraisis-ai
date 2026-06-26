bfe2499ae336b7a03536dbd5bc9bb06c2be37125
Arthi Kadalabalu Matha <arthikadalabalumatha@Arthis-MacBook-Air.local>
2026-06-25 19:26:17 -0700

    v0.8 - working UI

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
 import pickle
-import numpy as np
 
 app = Flask(__name__)
 
-# load model, scaler, and top genes
+# load model and top genes (no scaler needed!)
 with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 
-with open("scaler.pkl", "rb") as f:
-    scaler = pickle.load(f)
-#loading all the saved genes, the trained model, and scaler
 with open("top_genes.pkl", "rb") as f:
     top_genes = pickle.load(f)
 
@@ -27,13 +23,6 @@ def get_risk_label(prob):
     else:
         return "Very high risk of psoriasis"
 
-def score_patient(patient_data):
-    patient_median = patient_data.median(axis=1).values[0]
-    patient_centered = patient_data - patient_median
-    patient_scaled = scaler.transform(patient_centered)
-    prob = model.predict_proba(patient_scaled)[0][1]
-    return float(prob)
-#when the webpage sends a file like the patient data to analyze, it will run this 
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -42,14 +31,9 @@ def home():
 def analyze():
     file = request.files["file"]
     df = pd.read_csv(file, index_col=0)
-    
-    #onlythe 300 genes
     patient = df[top_genes]
-    
-    #output the risk scores and labels 
-    prob = score_patient(patient)
+    prob = float(model.predict_proba(patient)[0][1])
     label = get_risk_label(prob)
-    
     return jsonify({
         "probability": round(prob * 100, 1),
         "label": label
